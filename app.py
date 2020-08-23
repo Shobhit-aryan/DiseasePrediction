c920b0cf01543029e064cadf1aae0b2881e4ce04
Shobhit Aryan Lal <55244666+Shobhit-aryan@users.noreply.github.com>
2020-08-23 23:54:59 +0530

    updated

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 17:39:01 2020
+
+@author: Asus
+"""
+from flask import Flask, render_template, request
+import pickle
+
+classifier = pickle.load(open('model.pkl', 'rb'))
+cv = pickle.load(open('cv-transform.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method=='POST':
+        input_symptoms = request.form['input_symptoms']
+        data = [input_symptoms]
+        vect = cv.transform(data).toarray()
+        my_prediction = classifier.predict(vect)
+        return render_template('index.html',prediction=my_prediction)
+    
+if __name__=='__main__':
+    app.run(debug=True)
