5cfd4ffa654a457dde85de077ff9fbd917284ec3
Shobhit Aryan Lal <55244666+Shobhit-aryan@users.noreply.github.com>
2020-10-02 23:59:42 +0530

    corrected dropdown

--- app.py
+++ app.py
@@ -16,97 +16,105 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/symptom.html')
+def symp():
+    return render_template('symptom.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method=='POST':
-        input_symptoms = request.form['input_symptoms']
+        symptom1 = request.form['symp1']
+        symptom2 = request.form['symp2']
+        symptom3 = request.form['symp3']
+        symptom4 = request.form['symp4']
+        input_symptoms = symptom1+" "+symptom2+" "+symptom3+" "+symptom4
         data = [input_symptoms]
         vect = cv.transform(data).toarray()
         my_prediction = classifier.predict(vect)
         if(my_prediction==0):
-            return render_template('index.html', prediction="Paroymsal Positional Vertigo")
+            return render_template('symptom.html', prediction="Paroymsal Positional Vertigo")
         elif (my_prediction == 1):
-            return render_template('index.html', prediction="AIDS")
+            return render_template('symptom.html', prediction="AIDS")
         elif (my_prediction == 2):
-            return render_template('index.html', prediction="Acne")
+            return render_template('symptom.html', prediction="Acne")
         elif (my_prediction == 3):
-            return render_template('index.html', prediction="Alcoholic hepatitis")
+            return render_template('symptom.html', prediction="Alcoholic hepatitis")
         elif (my_prediction == 4):
-            return render_template('index.html', prediction="Allergy")
+            return render_template('symptom.html', prediction="Allergy")
         elif (my_prediction == 5):
-            return render_template('index.html', prediction="Arthritis")
+            return render_template('symptom.html', prediction="Arthritis")
         elif (my_prediction == 6):
-            return render_template('index.html', prediction="Bronchial Asthma")
+            return render_template('symptom.html', prediction="Bronchial Asthma")
         elif (my_prediction == 7):
-            return render_template('index.html', prediction="Cervical spondylosis")
+            return render_template('symptom.html', prediction="Cervical spondylosis")
         elif (my_prediction == 8):
-            return render_template('index.html', prediction="Chicken pox")
+            return render_template('symptom.html', prediction="Chicken pox")
         elif (my_prediction == 9):
-            return render_template('index.html', prediction="Chronic cholestasis")
+            return render_template('symptom.html', prediction="Chronic cholestasis")
         elif (my_prediction == 10):
-            return render_template('index.html', prediction="Common Cold")
+            return render_template('symptom.html', prediction="Common Cold")
         elif (my_prediction == 11):
-            return render_template('index.html', prediction="Dengue")
+            return render_template('symptom.html', prediction="Dengue")
         elif (my_prediction == 12):
-            return render_template('index.html', prediction="Diabetes")
+            return render_template('symptom.html', prediction="Diabetes")
         elif (my_prediction == 13):
-            return render_template('index.html', prediction="Dimorphic hemmorhoids(piles)")
+            return render_template('symptom.html', prediction="Dimorphic hemmorhoids(piles)")
         elif (my_prediction == 14):
-            return render_template('index.html', prediction="Drug Reaction")
+            return render_template('symptom.html', prediction="Drug Reaction")
         elif (my_prediction == 15):
-            return render_template('index.html', prediction="Fungal infection")
+            return render_template('symptom.html', prediction="Fungal infection")
         elif (my_prediction == 16):
-            return render_template('index.html', prediction="GERD")
+            return render_template('symptom.html', prediction="GERD")
         elif (my_prediction == 17):
-            return render_template('index.html', prediction="Gastroenteritis")
+            return render_template('symptom.html', prediction="Gastroenteritis")
         elif (my_prediction == 18):
-            return render_template('index.html', prediction="Heart attack")
+            return render_template('symptom.html', prediction="Heart attack")
         elif (my_prediction == 19):
-            return render_template('index.html', prediction="Hepatitis B")
+            return render_template('symptom.html', prediction="Hepatitis B")
         elif (my_prediction == 20):
-            return render_template('index.html', prediction="Hepatitis C")
+            return render_template('symptom.html', prediction="Hepatitis C")
         elif (my_prediction == 21):
-            return render_template('index.html', prediction="Hepatitis D")
+            return render_template('symptom.html', prediction="Hepatitis D")
         elif (my_prediction == 22):
-            return render_template('index.html', prediction="Hepatitis E")
+            return render_template('symptom.html', prediction="Hepatitis E")
         elif (my_prediction == 23):
-            return render_template('index.html', prediction="Hypertension")
+            return render_template('symptom.html', prediction="Hypertension")
         elif (my_prediction == 24):
-            return render_template('index.html', prediction="Hyperthyroidism")
+            return render_template('symptom.html', prediction="Hyperthyroidism")
         elif (my_prediction == 25):
-            return render_template('index.html', prediction="Hypoglycemia")
+            return render_template('symptom.html', prediction="Hypoglycemia")
         elif (my_prediction == 26):
-            return render_template('index.html', prediction="Hypothyroidism")
+            return render_template('symptom.html', prediction="Hypothyroidism")
         elif (my_prediction == 27):
-            return render_template('index.html', prediction="Impetigo")
+            return render_template('symptom.html', prediction="Impetigo")
         elif (my_prediction == 28):
-            return render_template('index.html', prediction="Jaundice")
+            return render_template('symptom.html', prediction="Jaundice")
         elif (my_prediction == 29):
-            return render_template('index.html', prediction="Malaria")
+            return render_template('symptom.html', prediction="Malaria")
         elif (my_prediction == 30):
-            return render_template('index.html', prediction="Migraine")
+            return render_template('symptom.html', prediction="Migraine")
         elif (my_prediction == 31):
-            return render_template('index.html', prediction="Osteoarthristis")
+            return render_template('symptom.html', prediction="Osteoarthristis")
         elif (my_prediction == 32):
-            return render_template('index.html', prediction="Paralysis (brain hemorrhage)")
+            return render_template('symptom.html', prediction="Paralysis (brain hemorrhage)")
         elif (my_prediction == 33):
-            return render_template('index.html', prediction="Peptic ulcer diseae")
+            return render_template('symptom.html', prediction=input_symptoms)
         elif (my_prediction == 34):
-            return render_template('index.html', prediction="Pneumonia")
+            return render_template('symptom.html', prediction="Pneumonia")
         elif (my_prediction == 35):
-            return render_template('index.html', prediction="Psoriasis")
+            return render_template('symptom.html', prediction="Psoriasis")
         elif (my_prediction == 36):
-            return render_template('index.html', prediction="Tuberculosis")
+            return render_template('symptom.html', prediction="Tuberculosis")
         elif (my_prediction == 37):
-            return render_template('index.html', prediction="Typhoid")
+            return render_template('symptom.html', prediction="Typhoid")
         elif (my_prediction == 38):
-            return render_template('index.html', prediction="Urinary tract infection")
+            return render_template('symptom.html', prediction="Urinary tract infection")
         elif (my_prediction == 39):
-            return render_template('index.html', prediction="Varicose veins")
+            return render_template('symptom.html', prediction="Varicose veins")
         elif (my_prediction == 40):
-            return render_template('index.html', prediction="hepatitis A")
+            return render_template('symptom.html', prediction="hepatitis A")
         else:
-            return render_template('index.html', prediction="Sorry Can't Predict")
+            return render_template('symptom.html', prediction="Sorry Can't Predict")
     
 if __name__=='__main__':
     app.run(debug=True)
